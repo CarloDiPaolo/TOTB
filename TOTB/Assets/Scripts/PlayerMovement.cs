c9d79f2bd42b4ee62c5c2791a834ec19f5f668ac
Carlo di Paolo <carlo.dipaolo94@gmail.com>
2025-08-16 18:52:47 +0200

    started PLayerMovement script

---
+++ TOTB/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+}
