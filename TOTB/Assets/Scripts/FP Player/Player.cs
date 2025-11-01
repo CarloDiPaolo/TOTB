2dd9daf55f4e0c4136935e231e37664e614432f0
Carlo di Paolo <carlo.dipaolo94@gmail.com>
2025-11-01 22:10:54 +0100

    moselook working

--- TOTB/Assets/Scripts/FP Player/Player.cs
+++ TOTB/Assets/Scripts/FP Player/Player.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using System;
 
-public class Player : MonoBehaviour
+
+
+
+namespace FleischWolf
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [RequireComponent(typeof(FPController))]
+    public class Player : MonoBehaviour
     {
-        
-    }
+        [Header("Components")]
+        [SerializeField] FPController FPController;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        #region Input Handling
+
+        void OnMove(InputValue value)
+        {
+            FPController.moveInput = value.Get<Vector2>();
+
+        }
+
+        void OnLook(InputValue value)
+        {
+            FPController.lookInput = value.Get<Vector2>();
+        }
+
+        #endregion
+
+        #region Unity Methods
+        void OnValidate()
+        {
+            if (FPController == null) FPController = GetComponent<FPController>();
+        }
+
+        void Start()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        #endregion
     }
 }
