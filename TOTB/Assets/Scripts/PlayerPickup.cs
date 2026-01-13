32d137bc9d0bb9ca27481d132ffe49f54e26b0e2
Carlo di Paolo <carlo.dipaolo94@gmail.com>
2026-01-13 23:05:53 +0100

    Setting up new Pickup script

--- TOTB/Assets/Scripts/PlayerPickup.cs
+++ TOTB/Assets/Scripts/PlayerPickup.cs
@@ -4,11 +4,24 @@ using UnityEngine.InputSystem;
 
 namespace FleischWolf
 {
+    
     public class PlayerPickup : MonoBehaviour
     {
-     public void OnGrab()
-        {
-            Debug.Log("Grab Object");
-        }
+        [SerializeField] Transform playerCamTransform; 
+        [SerializeField] LayerMask pickupLayerMask;
+        [SerializeField] Transform grabParentTransform;  
+        public float maxGrabDistance = 0.3f;
+        public void OnGrab()
+            {
+               // Debug.Log("Grab Object");
+
+                if (Physics.Raycast(playerCamTransform.position, playerCamTransform.forward, out RaycastHit raycastHit, maxGrabDistance, pickupLayerMask))
+            {
+                if(raycastHit.transform.TryGetComponent(out Pickup objectGrabbable))
+                {
+                    Debug.Log(raycastHit.transform);
+                }
+            }
+            }
     }
 }
